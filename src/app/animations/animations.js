5a419dddae81fddce514bea17f8e597ea69a76c6
LucaArgentieri <lucaargentieri-98@hotmail.it>
2021-06-08 15:22:02 +0200

    Update

--- src/app/animations/animations.js
+++ src/app/animations/animations.js
@@ -1,6 +1,8 @@
 import { gsap, Power1 } from "gsap";
 gsap.registerEase(Power1);
 
+const tl = gsap.timeline();
+
 export const selectOver = (el) => {
   gsap.to(el.target, {
     duration: 0.3,
@@ -13,4 +15,97 @@ export const removeOver = (el) => {
     duration: 0.3,
     width: "50%",
   });
+};
+
+export const rotateLogo = () => {
+  gsap.fromTo(
+    ".home_container .logo",
+    {
+      opacity: 0,
+    },
+    {
+      duration: 2,
+      rotateZ: 180,
+      opacity: 1,
+    }
+  );
+};
+
+export const animateText = () => {
+  tl.from([".home_container .stonehenge"], {
+    zIndex: 999,
+  });
+
+  tl.fromTo(
+    [".home_container .title"],
+    {
+      opacity: 0,
+      y: 50,
+      zIndex: 998,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+    }
+  );
+
+  tl.fromTo(".home_container .subtitle", {
+    opacity: 0,
+  }, {
+    opacity: 1,
+    duration: 1
+  });
+
+  tl.to([".home_container .stonehenge"], {
+    zIndex: 996,
+  });
+
+  tl.fromTo(
+    ".home_container .enter_btn",
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1,
+    }
+  );
+
+  tl.fromTo(
+    ".home_container .enter_btn .line",
+    {
+      opacity: 0,
+      x: -100,
+    },
+    {
+      opacity: 1,
+      x: 0,
+      duration: 1,
+    }
+  );
+};
+
+export const hoverBtn = () => {
+  gsap.fromTo(
+    ".home_container .enter_btn .line",
+    {
+      x: 0,
+    },
+    {
+      x: 100,
+    }
+  );
+};
+
+export const removeHoverBtn = () => {
+  gsap.fromTo(
+    ".home_container .enter_btn .line",
+    {
+      x: 100,
+    },
+    {
+      x: 1,
+    }
+  );
 };
--- src/app/view/home.js
+++ src/app/view/home.js
@@ -1,27 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../assets/svg/logo.svg";
 import Stonehenge from "../assets/images/STONEHENGE.png";
 import CanvasSky from "../components/canvasSky";
 import MovingStars from "../components/movingStars";
+import {
+  rotateLogo,
+  animateText,
+  hoverBtn,
+  removeHoverBtn,
+} from "../animations/animations";
 
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      rotateLogo();
+      animateText();
+    } else {
+      rotateLogo();
+    }
+  }, [isLoading]);
+
+  setTimeout(() => {
+    setIsLoading(true);
+  }, 3000);
+
   return (
     <div className="home">
       <div className="home_container flex flex_center flex_column">
-        <img className="logo" src={Logo} alt="logo" />
-        <p className="title c-white fs-96 nobleman upcase">in the dark</p>
-        <p className="subtitle athiti regular c-white fs-36">
-          How the ancients discovered astronomy
-        </p>
-        <Link to="/" className="enter_btn fs-36 c-white nobleman">
-          Enter
-        </Link>
-        <span className="stonehenge">
-          <img src={Stonehenge} alt="" />
-        </span>
+        {isLoading ? (
+          <>
+            <img className="logo" src={Logo} alt="logo" />
+            <p className="title c-white fs-96 nobleman upcase">in the dark</p>
+            <p className="subtitle athiti regular c-white fs-36">
+              How the ancients discovered astronomy
+            </p>
+            <Link
+              onMouseEnter={hoverBtn}
+              onMouseLeave={removeHoverBtn}
+              to="/"
+              className="enter_btn fs-36 c-white nobleman"
+            >
+              Enter
+              <span className="line"></span>
+            </Link>
+            <span className="stonehenge">
+              <img src={Stonehenge} alt="" />
+            </span>
+          </>
+        ) : (
+          <img className="logo" src={Logo} alt="logo" />
+        )}
       </div>
+
       <CanvasSky />
       <MovingStars />
     </div>
