987a0b14fbeff60e11c128198347ce8ea1a0589a
LucaArgentieri <Lucaargentieri-98@hotmail.it>
2021-06-22 10:57:15 +0200

    update

--- src/app/components/scenaPunto.js
+++ src/app/components/scenaPunto.js
@@ -33,65 +33,67 @@ const ScenaPunto = ({ id, type }) => {
 
   return (
     <div className="a-bcg-body flex flex_center flex_column c-black" id={id}>
-      <span onClick={closePopup}>
-        <h3 className="a-title nobleman regular">Return to model</h3>
-      </span>
+      <div className="a-bcg-body-container">
+        <span onClick={closePopup}>
+          <h3 className="a-title nobleman regular">Return to model</h3>
+        </span>
 
-      <div className="container">
-        <div className="row">
-          <h3 className="title nobleman bold fs-36 upcase">
-            {type === "stonehenge"
-              ? textData.stonehenge[id].title
-              : textData.pyramid[id].title}
-          </h3>
+        <div className="container">
+          <div className="row">
+            <h3 className="title nobleman bold fs-36 upcase">
+              {type === "stonehenge"
+                ? textData.stonehenge[id].title
+                : textData.pyramid[id].title}
+            </h3>
+          </div>
         </div>
+
+        <img
+          src={
+            type === "stonehenge"
+              ? textData.stonehenge[id].upperImage
+              : textData.pyramid[id].upperImage
+          }
+          className="img-position"
+        />
+
+        <p className="txt-position athiti medium">
+          {type === "stonehenge"
+            ? textData.stonehenge[id].desc1
+            : textData.pyramid[id].desc1}
+        </p>
+
+        <img
+          src={
+            type === "stonehenge"
+              ? textData.stonehenge[id].image
+              : textData.pyramid[id].image
+          }
+          className="img-position"
+        />
+
+        <img
+          src={
+            type === "stonehenge"
+              ? textData.stonehenge[id].image2
+              : textData.pyramid[id].image2
+          }
+          className="img-position"
+        />
+
+        <p className="txt-position athiti medium">
+          {type === "stonehenge"
+            ? textData.stonehenge[id].desc2
+            : textData.pyramid[id].desc2}
+        </p>
+
+        <p className="n-position athiti medium">
+          {id}/
+          {type === "stonehenge"
+            ? textData.stonehenge.length
+            : textData.pyramid.length}
+        </p>
       </div>
-
-      <img
-        src={
-          type === "stonehenge"
-            ? textData.stonehenge[id].upperImage
-            : textData.pyramid[id].upperImage
-        }
-        className="img-position"
-      />
-
-      <p className="txt-position athiti medium">
-        {type === "stonehenge"
-          ? textData.stonehenge[id].desc1
-          : textData.pyramid[id].desc1}
-      </p>
-
-      <img
-        src={
-          type === "stonehenge"
-            ? textData.stonehenge[id].image
-            : textData.pyramid[id].image
-        }
-        className="img-position"
-      />
-
-      <img
-        src={
-          type === "stonehenge"
-            ? textData.stonehenge[id].image2
-            : textData.pyramid[id].image2
-        }
-        className="img-position"
-      />
-
-      <p className="txt-position athiti medium">
-        {type === "stonehenge"
-          ? textData.stonehenge[id].desc2
-          : textData.pyramid[id].desc2}
-      </p>
-
-      <p className="n-position athiti medium">
-        {id}/
-        {type === "stonehenge"
-          ? textData.stonehenge.length
-          : textData.pyramid.length}
-      </p>
     </div>
   );
 };
